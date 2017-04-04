96bad73c74f66e39531b04144d19ffa3be779c8f
Roman Ustiantcev <liberrom.hse@gmail.com>
2017-04-04 20:04:46 +0300

    Test finished

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,12 +2,40 @@ import Vapor
 
 let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+drop.get { request in
+    return try JSON(node: [
+        "message":"hello vapor"
+        ])
 }
 
-drop.resource("posts", PostController())
+
+drop.get("hello") { request in
+    return try JSON(node: [
+        "message":"tired to say hello"
+        ])
+}
+
+drop.get("hello", "there") { request in
+    return try JSON(node: [
+        "message":"tired to say hello -_-"
+        ])
+}
+
+drop.get("beers", Int.self) { request, beers in
+    return try JSON(node: [
+        "message":"take down, pass ot around \(beers - 1) bottles of the beer on the wall..."
+        ])
+
+}
+
+drop.post("post") { request in
+    guard let name = request.data["name"]?.string else {
+        throw Abort.badRequest
+    }
+    
+    return try JSON(node: [
+        "message":"the name posted is \(name)"
+        ])
+}
 
 drop.run()
